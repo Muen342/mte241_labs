f379ab4c60d58f1937e433000c23927e03ad925a
mzhang <muen2001@hotmail.com>
2020-07-02 11:53:03 -0400

    lab 3 first commit

---
+++ lab_1/main.c
@@ -0,0 +1,101 @@
+/****************************************************************************
+ * This code is copyright (c) 2017 by the University of Waterloo and may    *
+ * not be redistributed without explicit written permission.                *
+ ****************************************************************************/
+ 
+#include <LPC17xx.h>
+#include "stdio.h"
+#include "uart.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "ctype.h"
+//#include <cmsis_os2.h>
+#define part_1
+
+int prevbutton = 1;
+int prevstate = 0;
+
+void button(void *arg){
+	LPC_GPIO2->FIODIR = 0x1f<<2; // Configure pins 2 to 6 on Port 0 as Output
+	LPC_GPIO1->FIODIR=0xf<<28;//configure pin 28-31 as output led and set to low
+	while(1){
+		if((LPC_GPIO2->FIOPIN & (1<<10)) == 0){
+			prevbutton = 0;
+		}
+		else{
+			if(prevbutton == 0 && prevstate == 0){
+				LPC_GPIO2->FIOSET = 1 << 6; // Output HIGH
+				prevstate = 1;
+			}
+			else if(prevbutton == 0 && prevstate == 1){
+				LPC_GPIO2->FIOCLR = 1 << 6; // Output LOW
+				prevstate = 0;
+			}
+			prevbutton = 1;
+		}
+		//osdelay
+	}
+}
+
+void ADC(void *arg){
+	LPC_SC->PCONP |= 1 << 12;
+	LPC_PINCON->PINSEL1 &= ~(0x03 <<18);//clear this value
+	LPC_PINCON->PINSEL1 |= (0x01<<18);//set it as adc ad0.2
+	LPC_ADC->ADCR |= 4 << 8;//divide by 4+1 so 5 mhz
+	LPC_ADC->ADCR |= 1 <<21;//enable circuit
+	LPC_ADC->ADCR |= 1 << 2;//sets channel of input
+	LPC_ADC -> ADCR &= ~1;//bit 1 was on for some reason
+	float val = 0;
+	while(1){
+		LPC_ADC->ADCR |= 1 <<24;//start conversion
+		if((LPC_ADC->ADGDR & (1<<31)) == 0){
+		}
+		else{
+			val = LPC_ADC->ADGDR >> 4 & 0xfff;
+			val /= 1241;
+			printf("%f\n", val);
+		}
+	}
+}
+
+void joystick(void *arg){
+	while (1) {
+		if((LPC_GPIO1->FIOPIN & (1 << 20)) == 0) //  PRESSED
+		{
+			if (((LPC_GPIO1->FIOPIN & (1 << 23)) == 0)) // NORTH
+				printf("North, Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 24)) == 0)) // EAST
+				printf("East, Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 25)) == 0)) // SOUTH
+				printf("South, Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 26)) == 0)) // WEST
+				printf("West, Pressed\n"); 
+			else
+			printf("Pressed centered\n"); // PRESSED, NO JOYSTICK MOVEMENT 
+		}
+		else if ((LPC_GPIO1->FIOPIN & (1 << 20)) != 0)
+		{
+			if (((LPC_GPIO1->FIOPIN & (1 << 23)) == 0)) // NORTH
+				printf("North, Not Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 24)) == 0)) // EAST
+				printf("East, Not Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 25)) == 0)) // SOUTH
+				printf("South, Not Pressed\n");
+			else if (((LPC_GPIO1->FIOPIN & (1 << 26)) == 0)) // WEST
+				printf("West, Not Pressed\n"); 
+			else
+			printf(" Not Pressed centered\n"); // NOT PRESSED, NO JOYSTICK MOVEMENT
+		}
+	}
+}
+
+int main( void ) 
+{
+	LPC_GPIO2->FIODIR = 0x1f<<2; // Configure pins 2 to 6 on Port 0 as Output
+	LPC_GPIO1->FIODIR=0xf<<28;//configure pin 28-31 as output led and set to low
+	//osKernelInitialize();
+	//osThreadNew(button, NULL, NULL);
+	//osThreadNew(ADC, NULL, NULL);
+	//osThreadNew(joystick, NULL, NULL);
+	//osKernelStart();
+}
