e10b286227d38e2cbf2e4fc07ca38d383671758a
mzhang <muen2001@hotmail.com>
2020-07-19 21:16:05 -0400

    fixed errors

--- lab_1/main.c
+++ lab_1/main.c
@@ -13,11 +13,10 @@
 #include <rtx_os.h>
 #include "random.h"
 #include "lfsr113.h"
-int TPS = 268451096;//ticks per second
 int ARRIVALRATE = 9;
 int SERVICERATE = 10;
 int QSIZE = 10;
-int msg = NULL;
+int msg = 0;
 int currentClient = 1;
 int MAXCLIENTS = 	2;
 int totalRecieved1 = 0;
@@ -26,35 +25,26 @@ int totalSent1 = 0;
 int totalRecieved2 = 0;
 int totalOverflow2 = 0;
 int totalSent2 = 0;
+int currentTime = 0;
+osMessageQueueId_t q1;
+osMessageQueueId_t q2;
+
 uint32_t getTicks(rate){
 	uint32_t nevent = next_event();
-	nevent = nevent*TPS/rate;
+	nevent = nevent*osKernelGetTickFreq()/rate;
 	nevent = (nevent>>16);
 	return nevent;
 }
 
-osMessageQueueId_t createNewQ(){
-	osMessageQueueId_t q_id = osMessageQueueNew(QSIZE, sizeof(int), NULL);
-	return q_id;
-}
-
-void monitor(void *arg){
-	while(1){
-		printf("Queue 1:\nTotal Received: %i\nTotal Sent: %i\nTotal Overflow: %i\n", totalRecieved1, totalSent1, totalOverflow1);// displays monitored values for q1 and q2
-		printf("Queue 2:\nTotal Received: %i\nTotal Sent: %i\nTotal Overflow: %i\n", totalRecieved2, totalSent2, totalOverflow2);
-		osDelay(TPS);
-	}
-}
 void client(void *arg){
-	osMessageQueueId_t q1 = &arg[0];  // assigns q1 and q2 arg array positions
-	osMessageQueueId_t q2 = &arg[1];
 	while(1){
+		osDelay(getTicks(ARRIVALRATE*MAXCLIENTS));//because they are both in this loop it needs to be multiplied also yields to the other threads
 		if(currentClient == MAXCLIENTS){
 			osStatus_t stat = osMessageQueuePut(q2, &msg, 0, 0); // status of q2 as message is sent to q2
 			if(stat == osErrorResource){
 				totalOverflow2++;
 			}
-			else{
+			else if(stat == osOK){
 				totalSent2++;
 			}
 			currentClient = 1;
@@ -64,29 +54,44 @@ void client(void *arg){
 			if(stat == osErrorResource){
 				totalOverflow1++;// inc overflow val
 			}
-			else{
+			else if(stat == osOK){
 				totalSent1++;
 			}
 			currentClient++;
 		}
-		osDelay(getTicks(ARRIVALRATE*MAXCLIENTS));//because they are both in this loop it needs to be multiplied
+		osThreadYield();
+	}
+}
+void monitor(void *arg){
+	while(1){
+		osDelay(osKernelGetTickFreq());
+		currentTime++;
+		printf("Current Time: %is\n", currentTime);
+		printf("Queue 1:  Total Received: %i  Total Sent: %i  Total Overflow: %i\n", totalRecieved1, totalSent1, totalOverflow1);// displays monitored values for q1 and q2
+		printf("Queue 2:  Total Received: %i  Total Sent: %i  Total Overflow: %i\n", totalRecieved2, totalSent2, totalOverflow2);
+		osThreadYield();
 	}
 }
 void server(void *arg){
 	while(1){
-		osMessageQueueGet(&arg, &msg, NULL, osWaitForever);
-		//find way to find out which q it is servicing
-		totalRecieved1++;
 		osDelay(getTicks(SERVICERATE));
+		osMessageQueueGet(arg, &msg, NULL, osWaitForever);
+		//find way to find out which q it is servicing
+		if(arg == q1){
+			totalRecieved1++;
+		}
+		else if(arg == q2){
+			totalRecieved2++;
+		}
+		osThreadYield();
 	}
 }
 int main( void ) 
 {
-	osMessageQueueId_t q1 = createNewQ();
-	osMessageQueueId_t q2 = createNewQ();
-	osMessageQueueId_t clients[2] = {q1, q2};
 	osKernelInitialize();
-	osThreadNew(client, clients, NULL);
+	q1 = osMessageQueueNew(QSIZE, sizeof(int), NULL);//initialize them
+	q2 = osMessageQueueNew(QSIZE, sizeof(int), NULL);
+	osThreadNew(client, NULL, NULL);
 	osThreadNew(server, q1, NULL);
 	osThreadNew(server, q2, NULL);
 	osThreadNew(monitor, NULL, NULL);
